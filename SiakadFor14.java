33baf9bb1f26ddd3abc22d9e84bbaf1012ccad5d
Galih1004 <mochgalihputrapratama@gmail.com>
2025-10-22 10:56:39 +0700

    Modifikasi untuk pertanyaan 4

--- SiakadFor14.java
+++ SiakadFor14.java
@@ -1,9 +1,11 @@
 import java.util.Scanner;
 
-public class SiakadFor {
+public class SiakadFor14 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double nilai, Tertinggi = 0, Terendah = 100;
+        int lulus = 0;
+        int tidakLulus = 0;
 
         for (int i = 1; i <= 10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
@@ -15,9 +17,16 @@ public class SiakadFor {
             if (nilai < Terendah) {
                 Terendah = nilai;
             }
+            if (nilai > 60) {
+               lulus++;
+            } else {
+               tidakLulus++;
+            }
         }
             System.out.println("Nilai tertinggi :" + Tertinggi);
             System.out.println("Nilai terendah :" + Terendah);
+            System.out.println("Jumlah lulus :" + lulus);
+            System.out.println("Jumlah tidak lulus :" + tidakLulus);
 
         scanner.close();
     }
