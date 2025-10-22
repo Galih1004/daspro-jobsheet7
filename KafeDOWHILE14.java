07c6d1347afe95a9678e79b0bf47ff849245c553
Galih1004 <mochgalihputrapratama@gmail.com>
2025-10-22 13:10:32 +0700

    percobaan commit DOWHILE

--- KafeDOWHILE14.java
+++ KafeDOWHILE14.java
@@ -1,29 +1,29 @@
 import java.util.Scanner;
 
-public class KafeDOWHILE14 {
+public class kafeDoWhile14 {
     public static void main(String[] args) {
-
-       Scanner sc=new Scanner(System.in); 
-       int kopi, teh, roti;
-       String namaPelanggan;
-       int hargaKopi=12000,hargaTeh=7000,hargaRoti=20000;
-
-       do {
-            System.out.print("Masukkan nama pelanggan (Ketik 'Batal' Untuk Membatalkan): ");
-            namaPelanggan=sc.nextLine();
-            if(namaPelanggan.equalsIgnoreCase("Batal")){
-                System.out.println("Transaksi dibatalkan.");
+        Scanner sc = new Scanner (System.in);
+        int kopi, teh, roti, hargaKopi = 12000, hargaTeh=7000, hargaRoti = 20000, totalHarga;
+        String namaPelanggan;
+        do{
+            System.out.println("masukkan nama pelanggan (ketik 'batal' untuk keluar)");
+            namaPelanggan = sc.nextLine();
+            if(namaPelanggan.equalsIgnoreCase("batal")){
+                System.out.println("Transaksi dibatalkan. ");
                 break;
             }
-            System.out.print("Masukkan jumlah kopi yang dipesan: ");
-            kopi=sc.nextInt();
-            System.out.print("Masukkan jumlah teh yang dipesan: ");
-            teh=sc.nextInt();
-            System.out.print("Masukkan jumlah roti yang dipesan: ");
-            roti=sc.nextInt();
-            int totalHarga=(kopi*hargaKopi)+(teh*hargaTeh)+(roti*hargaRoti);
-            System.out.println("Total harga untuk " + namaPelanggan + " adalah: Rp " + totalHarga);
-       } while (true);
-         sc.close();
+            System.out.print("jumlah kopi: ");
+            kopi = sc.nextInt();
+            System.out.print("jumlah teh: ");
+            teh = sc.nextInt();
+            System.out.print("jumlah roti: ");
+            roti = sc.nextInt();
+
+            totalHarga = (kopi*hargaKopi)+(teh*hargaTeh)+(roti*hargaRoti);
+            System.out.println("total yang harus dibayar: Rp "+totalHarga);
+            sc.nextLine();
+        }while(true);
+        System.out.println("semua transaksi selesai. ");
+        sc.close();
     }
 }
